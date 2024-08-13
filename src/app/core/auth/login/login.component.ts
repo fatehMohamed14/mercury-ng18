ef5a49d6b7127e7aec7f00b933c7f8720f0f55f2
fatehMohamed14 <fateh@octodet.com>
2024-08-13 11:49:31 +0100

    fix login style

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -1,16 +1,64 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthStore } from '../../../store/auth.store';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 
 @Component({
   selector: 'moh-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatOptionModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
   readonly store = inject(AuthStore);
+
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  submit() {
+    console.log(this.loginForm.value);
+  }
 
   ngOnInit(): void {
     this.store.login({ username: 'admin', password: 'P@ssw0rd' });
