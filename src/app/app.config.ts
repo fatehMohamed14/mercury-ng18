c247242d29b43323965769b6736aa0c0e53a0d9a
fatehMohamed14 <fateh@octodet.com>
2024-08-14 10:34:44 +0100

    update login view 🤡

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,11 +8,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(),
   ],
 };
