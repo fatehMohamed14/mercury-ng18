c2a982b852c66db7db8da05ff7e3a85293490fd7
fatehMohamed14 <fateh@octodet.com>
2024-08-15 01:44:28 +0100

    fix planets list card

--- src/app/features/planet/planets/planets.component.ts
+++ src/app/features/planet/planets/planets.component.ts
@@ -1,10 +1,21 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PlanetStore } from '../../../store/planet.store';
+import { CommonModule } from '@angular/common';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 
 @Component({
   selector: 'moh-planets',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatChipsModule,
+    FlexLayoutModule,
+  ],
   providers: [PlanetStore],
   templateUrl: './planets.component.html',
   styleUrl: './planets.component.scss',
